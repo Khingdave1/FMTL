88e14c48497a68e0788180661595e851951a215a
Khingdave <dheyved1@gmail.com>
2021-11-28 14:44:08 +0100

    Updated go to top button

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-default',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
-  constructor() { }
+  windowScrolled: boolean;
 
-  ngOnInit(): void {
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  // When user scroll 100 away from the top of the document
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    }
+    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = false;
+    }
+  }
+
+  ngOnInit(): void { }
+
+  // Scroll Up
+  scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 
 }
